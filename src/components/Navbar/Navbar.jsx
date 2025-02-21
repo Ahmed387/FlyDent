22a3d4432488849df6ae6f61f0bea58dd2864704
Daniels <www201820192020@gmail.com>
2025-02-21 22:13:42 +0200

    AdminPrblemfix1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -154,12 +154,12 @@ export default function Navbar() {
               </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Follow us
-              </a>
+              </Link>
             </li>
             {/* <li>
               <input
