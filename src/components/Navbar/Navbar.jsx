4a29479d81ebefe0a2165006903916f8cce386df
Daniels <www201820192020@gmail.com>
2025-02-21 23:01:36 +0200

    menu prob1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  // const [ishover, setishover] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const handleMouseEnter = () => {
-  //   setishover(true);
-  // };
-
-  // const handleMouseLeave = () => {
-  //   setishover(false);
-  // };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* زر القائمة للموبايل */}
         <button
-          data-collapse-toggle="navbar-dropdown"
+          onClick={toggleMenu}
           type="button"
           className="inline-flex border-2 border-blue-800 items-center p-2 w-10 h-10 justify-end text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-auto"
           aria-controls="navbar-dropdown"
-          aria-expanded="false"
+          aria-expanded={isOpen}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -38,7 +36,12 @@ export default function Navbar() {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+
+        {/* القائمة */}
+        <div
+          className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
+          id="navbar-dropdown"
+        >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
@@ -49,15 +52,6 @@ export default function Navbar() {
                 Home
               </Link>
             </li>
-
-            {/* <li>
-              <Link
-                to={"/ourservice"}
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                OurServices
-              </Link>
-            </li> */}
             <li>
               <Link
                 to={"/ourTeam"}
@@ -66,69 +60,6 @@ export default function Navbar() {
                 OurTeam
               </Link>
             </li>
-            {/* <li className="relative group">
-              <button
-                id="dropdownNavbarLink"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                data-dropdown-toggle="dropdownNavbar"
-                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-              >
-                Our-Service
-                <svg
-                  className="w-2.5 h-5 ms-2.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
-              </button>
-              {/* Dropdown menu */}
-            {/* <div
-                className={`z-10 ${
-                  ishover ? "block" : "hidden "
-                }   group-hover:block font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600 `}
-              >
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
-                  aria-labelledby="dropdownLargeButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Dashboard
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Earnings
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li> */}
-
             <li>
               <Link
                 to={"/reservation"}
@@ -153,21 +84,6 @@ export default function Navbar() {
                 Contact-us
               </Link>
             </li>
-            <li>
-              <Link
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Follow us
-              </Link>
-            </li>
-            {/* <li>
-              <input
-                type="text"
-                className="border border-gray-300 rounded-md pl-8 pr-3 py-1 focus:ring-2 focus:ring-blue-400"
-                placeholder="Search"
-              />
-            </li> */}
           </ul>
         </div>
       </div>
